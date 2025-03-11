8daa693e96455e7444caf1a85b9f9be8b786eb0b
ahmed amine maarouf <ahmedaminemaarouf1@gmail.com>
2025-03-11 02:07:17 +0000

    fixing the link

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://your-frontend-domain.com'], // Add frontend domain
+    origin: ['http://localhost:3000', 'https://platinium-admin.netlify.app/'], // Add frontend domain
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
